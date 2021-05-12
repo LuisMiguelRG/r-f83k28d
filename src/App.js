76c66d9229ef431c4d4a27159e468e1a47c1f88a
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-05-11 21:20:27 -0500

    incrementa RG

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        { this.state.show ? <span className="value">{ this.state.clicks }</span> : '' }
+        <button id="inc" onClick={this.IncrementItem}>Incrementa</button>
       </div>
     );
   }
